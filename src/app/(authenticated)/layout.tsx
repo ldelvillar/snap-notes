f34774028d91b7b6bc5eb47d48216914d0562f83
ldelvillar <ldelvillarvazquez@gmail.com>
2025-10-26 20:26:16 +0100

    fix: remove icon property in metadata

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -7,7 +7,6 @@ export const metadata: Metadata = {
     "Simplify Your Note-Taking with SnapNotes - Fast, Secure, and Organized",
   description:
     "Experience seamless note-taking with SnapNotes. Create, organize, and access your notes from anywhere with ease. Perfect for personal and professional use, SnapNotes ensures your notes stay secure and always at your fingertips.",
-  icons: "/logo.svg",
 };
 
 export default function NotesLayout({
